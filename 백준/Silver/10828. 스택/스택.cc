bdf6b4a4b7e1646b8de4e7b70ff10436262f2306
Sunyoung Ha <sunyoung.shay@gmail.com>
2026-03-25 21:50:20 +0900

    [Silver IV] Title: 스택, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    stack<int> st;
+    string cmd;
+
+    while (n--) {
+        cin >> cmd;
+
+        if (cmd == "push") {
+            int x;
+            cin >> x;
+            st.push(x);
+        }
+        else if (cmd == "pop") {
+            if (st.empty()) {
+                cout << -1 << '\n';
+            } else {
+                cout << st.top() << '\n';
+                st.pop();
+            }
+        }
+        else if (cmd == "size") {
+            cout << st.size() << '\n';
+        }
+        else if (cmd == "empty") {
+            cout << (st.empty() ? 1 : 0) << '\n';
+        }
+        else if (cmd == "top") {
+            if (st.empty()) {
+                cout << -1 << '\n';
+            } else {
+                cout << st.top() << '\n';
+            }
+        }
+    }
+
+    return 0;
+}
